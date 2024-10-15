d20624f7e4f0f272307f4fc3b837e0480e985057
anniemon <seojung19@gmail.com>
2024-10-16 00:45:24 +0900

    product 단건 조회 api 구현

--- src/application/controllers/product.controller.ts
+++ src/application/controllers/product.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProductFacade } from '@domain/usecase/product.facade';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productFacade: ProductFacade) {}
 
-  @Get()
-  async getProductByIdWithStock(@Query('productId') productId: number) {
+  @Get(':productId')
+  async getProductByIdWithStock(@Param('productId') productId: number) {
     return this.productFacade.getProductByIdWithStock(productId);
   }
 }
