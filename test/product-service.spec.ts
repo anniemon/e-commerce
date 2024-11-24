098cc3e27bc02391f97eaa526f9dcd452394d007
anniemon <seojung19@gmail.com>
2024-11-24 19:57:59 +0900

    test: 여러 상품 조회(재고 포함) 테스트 추가

--- test/product-service.spec.ts
+++ test/product-service.spec.ts
@@ -6,7 +6,9 @@ describe('ProductService', () => {
   let productService: ProductService;
   const productRepository = {
     getProductByIdWithStock: jest.fn().mockResolvedValue({ id: 1 }),
-    findProductsByIds: jest.fn().mockResolvedValue([{ id: 1 }, { id: 2 }]),
+    findProductsByIdsWithStock: jest
+      .fn()
+      .mockResolvedValue([{ id: 1 }, { id: 2 }]),
   };
   const stockRepository = {
     decrementStockWithLock: jest.fn().mockResolvedValue({ id: 1 }),
@@ -41,15 +43,18 @@ describe('ProductService', () => {
     expect(productRepository.getProductByIdWithStock).toHaveBeenCalledTimes(1);
   });
 
-  it('findProductsByIds: 상품 아이디 배열로 상품 조회 메서드를 호출해야 한다.', async () => {
+  it('findProductsByIdsWithStock: 상품 아이디 배열로 상품 조회 메서드를 호출해야 한다.', async () => {
     const productIds = [1, 2];
-    const products = await productService.findProductsByIds(productIds);
+    const products =
+      await productService.findProductsByIdsWithStock(productIds);
 
     expect(products.length).toBe(productIds.length);
-    expect(productRepository.findProductsByIds).toHaveBeenCalledWith(
+    expect(productRepository.findProductsByIdsWithStock).toHaveBeenCalledWith(
       productIds,
     );
-    expect(productRepository.findProductsByIds).toHaveBeenCalledTimes(1);
+    expect(productRepository.findProductsByIdsWithStock).toHaveBeenCalledTimes(
+      1,
+    );
   });
 
   it('decrementStockWithLock: 주문 상품 갯수만큼 상품 재고 차감 메서드를 호출해야 한다.', async () => {
