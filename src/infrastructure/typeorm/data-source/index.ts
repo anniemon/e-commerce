02a350ae8bfae0bb55d12c191950edecb8a1a7ac
anniemon <seojung19@gmail.com>
2024-11-24 16:29:44 +0900

    infra: 데이터 소스 호스트 로컬 참조하도록 변경

--- src/infrastructure/typeorm/data-source/index.ts
+++ src/infrastructure/typeorm/data-source/index.ts
@@ -3,11 +3,12 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 // TODO: 환경별 설정 분리
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: 'mysql',
+  host: '127.0.0.1',
   port: 3307,
-  username: 'anniemon',
-  password: 'anniemon',
-  database: 'e-commerce_local',
+  bigNumberStrings: false,
+  username: 'root',
+  password: 'root',
+  database: `e-commerce_local`,
   migrationsRun: true,
   entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
