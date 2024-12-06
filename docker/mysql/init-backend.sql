e796f91d1b618319be5740ab105cb49014b8062f
anniemon <seojung19@gmail.com>
2024-12-06 15:36:11 +0900

    misc: 데이터베이스명 snake case로 통일

--- docker/mysql/init-backend.sql
+++ docker/mysql/init-backend.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS `e-commerce_local` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-DROP DATABASE IF EXISTS `e-commerce_test`;
-CREATE DATABASE IF NOT EXISTS `e-commerce_test` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `e_commerce_local` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+DROP DATABASE IF EXISTS `e_commerce_test`;
+CREATE DATABASE IF NOT EXISTS `e_commerce_test` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 CREATE USER IF NOT EXISTS 'anniemon'@'%' IDENTIFIED BY 'anniemon';
